8582aafa8249d2551450c963c9c39b5be87a16d5
osborne7 <eleanor.c.osborne@gmail.com>
2020-06-14 17:15:32 -0400

    Complete phase 3

--- src/components/RetrieveData.jsx
+++ src/components/RetrieveData.jsx
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import Films from './Films';
+import People from './People';
+
+
 // import 'isomorphic-fetch';
 // import 'es6-promise';
 
@@ -8,7 +11,10 @@ class RetrieveData extends Component {
         super(props);
 
         this.state = {
-            films: []
+            films: [],
+            people: [],
+            filmsLoaded: false,
+            peopleLoaded: false
         };
     }
 
@@ -18,22 +24,84 @@ class RetrieveData extends Component {
         .then(films => {
             this.setState({films});
         });
+        fetch('https://ghibliapi.herokuapp.com/people')
+        .then(res => res.json())
+        .then(people => {
+            this.setState({people});
+        });
     }
-    render() {
-    return (
+
+    filmsLoaded() {
+        this.setState({ filmsLoaded: true });
+    }
+
+    peopleLoaded() {
+        this.setState({ peopleLoaded: true });
+    }
+
+    showFilms() {
+        return (
+        <main>
+        <img className="mx-auto d-block" src="https://ghibliapi.herokuapp.com/images/logo.svg" alt="studio ghibli logo"/>
         <div className="container-fluid justify-content-center">
+        <div className="row">
+            {this.state.films.map((film, index) => {
+                return (
+                    <div key={index} className="col-lg-4">
+                        <Films title={film.title} description={film.description} id={film.id} />
+                    </div>
+                )
+            })}
+        </div>
+    </div>
+    </main>
+        )
+    }
+
+    showPeople() {
+        return (
+            <main>
+            <img className="mx-auto d-block" src="https://ghibliapi.herokuapp.com/images/logo.svg" alt="studio ghibli logo"/>
+            <div className="container-fluid justify-content-center">
             <div className="row">
-                {this.state.films.map((film, index) => {
+                {this.state.people.map((person, index) => {
                     return (
                         <div key={index} className="col-lg-4">
-                            <Films title={film.title} description={film.description} id={film.id} />
+                            <People name={person.name} age={person.age} gender={person.gender} />
                         </div>
                     )
                 })}
             </div>
-
         </div>
+        </main>
+            )
+    }
+
+    initialView() {
+        return (
+            <div>
+                <img className="mx-auto d-block" src="https://ghibliapi.herokuapp.com/images/logo.svg" alt="studio ghibli logo"/>
+                <button className="btn btn-block btn-lg btn-outline-primary" onClick={() => this.filmsLoaded()}>Show Films</button>
+                <br/>
+                <button className="btn btn-block btn-lg btn-outline-primary" onClick={() => this.peopleLoaded()}>Show People</button>
+            </div>
         )
+    }
+
+    render() {
+        if (this.state.filmsLoaded) {
+            return (
+            <div>{this.showFilms()}</div>
+            )
+        } else if (this.state.peopleLoaded) {
+            return (
+            <div>{this.showPeople()}</div>
+            )
+        } else {
+            return (
+            <div>{this.initialView()}</div>
+            )
+        }
     }
 }
 
